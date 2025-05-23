be13691e3c178c7555c6165bea4320dde1a7a5a0
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-05-23 03:40:19 +0000

    install typescript globally and compiling the main.ts file

---
+++ 0x04-TypeScript/task_1/dist/js/main.js
@@ -0,0 +1,40 @@
+;
+var teacher3 = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
+;
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
+;
+var printTeacher = function (firstName, lastName) {
+    return firstName.charAt(0) + ". " + lastName;
+};
+console.log(printTeacher("John", "Doe"));
+;
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    ;
+    StudentClass.prototype.workOnHomework = function () {
+        return "Currently working";
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
+;
+//# sourceMappingURL=main.js.map
---
+++ 0x04-TypeScript/task_1/dist/webpack.config.js
@@ -0,0 +1,43 @@
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+var ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+module.exports = {
+    mode: "development",
+    entry: "./js/main.ts",
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    devServer: {
+        // contentBase: "./dist",
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        hot: true,
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Development",
+            template: './js/index.html',
+        })
+    ],
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    }
+};
+//# sourceMappingURL=webpack.config.js.map
